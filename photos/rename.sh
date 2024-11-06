931332660bea51c3bb05b10c6644eff8733b8194
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2024-11-06 18:49:35 -0500

    add comment

--- photos/rename.sh
+++ photos/rename.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# get rid of spaces in filenames
+# including the unicode "narrow no-break space"
+# that OS X incredibly throws in there by default
+
 for f in *.png; do mv -- "$f" "${f//[!0-9a-zA-Z.-]/_}"; done
 
